02fbd6a12f40141b6c15dcd1f9db9fc4234e502c
Dong Quyet <quyetvdong@gmail.com>
2021-06-04 00:22:13 +0700

    added dbcontext, created products and categories models

---
+++ standardProject.Data/EF/EShopDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using standardProject.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace standardProject.Data.EF
+{
+  public class EShopDbContext : DbContext
+  {
+    public EShopDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+  }
+}
---
+++ standardProject.Data/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace standardProject.Data.Entities
+{
+  public class Product
+  {
+    public int Id { get;set;}
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public decimal OriginalPrice { get; set; }
+    public string Detail { get; set; }
+    public int Stock { get; set; }
+    public int ViewCount { get;set;}
+    public DateTime DateCreated { get; set; }
+    public DateTime DateModified { get;set;}
+    public string SEODescription { get; set; }
+    public string SEOTitle { get; set; }
+    public string SEOAlias { get;set;}
+
+  }
+}
